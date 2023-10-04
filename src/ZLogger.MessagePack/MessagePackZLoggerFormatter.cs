5174614cf2b826bc12f4f36726ff2981e9c7cffe
hadashiA <dev@hadashikick.jp>
2023-10-05 08:28:48 +0900

    Add ZLogger.MessagePack

---
+++ src/ZLogger.MessagePack/MessagePackZLoggerFormatter.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Buffers;
+using MessagePack;
+using Microsoft.Extensions.Logging;
+
+namespace ZLogger.MessagePack
+{
+    public static class ZLoggerOptionsMessagePackExtensions
+    {
+        public static ZLoggerOptions UseMessagePackFormatter(this ZLoggerOptions options, Action<MessagePackZLoggerFormatter> messagePackConfigure = null)
+        {
+            return options.UseFormatter(() =>
+            {
+                var formatter = new MessagePackZLoggerFormatter();
+                messagePackConfigure?.Invoke(formatter);
+                return formatter;
+            });
+        }
+    }
+
+    public class MessagePackZLoggerFormatter : IZLoggerFormatter
+    {
+        // "CategoryName"
+        static readonly byte[] CategoryNameKey = { 0b10100000 | 12, 67, 97, 116, 101, 103, 111, 114, 121, 78, 97, 109, 101 };
+        // "Timestamp"
+        static readonly byte[] TimestampKey = { 0b10100000 | 9, 84, 105, 109, 101, 115, 116, 97, 109, 112 };
+        // "LogLevel"
+        static readonly byte[] LogLevelKey = { 0b10100000 | 8, 76, 111, 103, 76, 101, 118, 101, 108 };
+        // "EventId"
+        static readonly byte[] EventIdKey = { 0b10100000 | 7, 69, 118, 101, 110, 116, 73, 100 };
+        // "EventIdName"
+        static readonly byte[] EventIdNameKey = { 0b10100000 | 11, 69, 118, 101, 110, 116, 73, 100, 78, 97, 109, 101 };
+        // "Exception"
+        static readonly byte[] ExceptionKey = { 0b10100000 | 9, 69, 120, 99, 101, 112, 116, 105, 111, 110 };
+
+        // "Name"
+        static readonly byte[] NameKey = { 0b10100000 | 4, 78, 97, 109, 101 };
+        // "Message"
+        static readonly byte[] MessageKey = { 0b10100000 | 7, 77, 101, 115, 115, 97, 103, 101 };
+        // "StackTrace"
+        static readonly byte[] StackTraceKey = { 0b10100000 | 10, 83, 116, 97, 99, 107, 84, 114, 97, 99, 101 };
+        // "InnerException"
+        static readonly byte[] InnerExceptionKey = { 0b10100000 | 14, 73, 110, 110, 101, 114, 69, 120, 99, 101, 112, 116, 105, 111, 110 };
+
+        // "Trace"
+        static readonly byte[] Trace = { 0b10100000 | 5, 84, 114, 97, 99, 101 };
+        // "Debug"
+        static readonly byte[] Debug = { 0b10100000 | 5, 68, 101, 98, 117, 103 };
+        // "Information"
+        static readonly byte[] Information = { 0b10100000 | 11, 73, 110, 102, 111, 114, 109, 97, 116, 105, 111, 110 };
+        // "Warning"
+        static readonly byte[] Warning = { 0b10100000 | 7, 87, 97, 114, 110, 105, 110, 103 };
+        // "Error"
+        static readonly byte[] Error = { 0b10100000 | 5, 69, 114, 114, 111, 114 };
+        // "Critical"
+        static readonly byte[] Critical = { 0b10100000 | 8, 67, 114, 105, 116, 105, 99, 97, 108 };
+        // "None"
+        static readonly byte[] None = { 0b10100000 | 4, 78, 111, 110, 101 };
+
+        public MessagePackSerializerOptions MessagePackSerializerOptions { get; set; } = MessagePackSerializerOptions.Standard;
+        public string MessagePropertyName { get; set; } = "Message";
+        public string PayloadPropertyName { get; set; } = "Payload";
+
+        public void FormatLogEntry<TEntry, TPayload>(
+            IBufferWriter<byte> writer,
+            TEntry entry,
+            TPayload payload,
+            ReadOnlySpan<byte> utf8Message)
+            where TEntry : IZLoggerEntry
+        {
+            var messagePackWriter = new MessagePackWriter(writer);
+            var hasPayload = payload != null;
+            var hasException = entry.LogInfo.Exception != null;
+
+            messagePackWriter.WriteMapHeader(6 + (hasException ? 1 : 0) + (hasPayload ? 1 : 0));
+
+            messagePackWriter.WriteRaw(CategoryNameKey);
+            messagePackWriter.Write(entry.LogInfo.CategoryName);
+
+            messagePackWriter.WriteRaw(LogLevelKey);
+            messagePackWriter.WriteRaw(EncodedLogLevel(entry.LogInfo.LogLevel));
+
+            messagePackWriter.WriteRaw(EventIdKey);
+            messagePackWriter.WriteInt32(entry.LogInfo.EventId.Id);
+
+            messagePackWriter.WriteRaw(EventIdNameKey);
+            messagePackWriter.Write(entry.LogInfo.EventId.Name);
+
+            messagePackWriter.WriteRaw(TimestampKey);
+            MessagePackSerializerOptions.Resolver.GetFormatterWithVerify<DateTimeOffset>()
+                .Serialize(ref messagePackWriter, entry.LogInfo.Timestamp, MessagePackSerializerOptions);
+
+            messagePackWriter.Write(MessagePropertyName);
+            messagePackWriter.WriteString(utf8Message);
+
+            if (hasException)
+            {
+                messagePackWriter.WriteRaw(ExceptionKey);
+                WriteException(ref messagePackWriter, entry.LogInfo.Exception);
+            }
+            if (hasPayload)
+            {
+                messagePackWriter.Write(PayloadPropertyName);
+                MessagePackSerializerOptions.Resolver.GetFormatterWithVerify<TPayload>()
+                    .Serialize(ref messagePackWriter, payload, MessagePackSerializerOptions);
+            }
+            messagePackWriter.Flush();
+        }
+
+        static void WriteException(ref MessagePackWriter messagePackWriter, Exception ex)
+        {
+            if (ex == null)
+            {
+                messagePackWriter.WriteNil();
+                return;
+            }
+
+            messagePackWriter.WriteMapHeader(4);
+
+            messagePackWriter.WriteRaw(NameKey);
+            messagePackWriter.Write(ex.GetType().FullName);
+
+            messagePackWriter.WriteRaw(MessageKey);
+            messagePackWriter.Write(ex.Message);
+
+            messagePackWriter.WriteRaw(StackTraceKey);
+            messagePackWriter.Write(ex.StackTrace);
+
+            messagePackWriter.WriteRaw(InnerExceptionKey);
+            WriteException(ref messagePackWriter, ex.InnerException);
+        }
+
+        static byte[] EncodedLogLevel(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                    return Trace;
+                case LogLevel.Debug:
+                    return Debug;
+                case LogLevel.Information:
+                    return Information;
+                case LogLevel.Warning:
+                    return Warning;
+                case LogLevel.Error:
+                    return Error;
+                case LogLevel.Critical:
+                    return Critical;
+                case LogLevel.None:
+                    return None;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
+            }
+        }
+    }
+}
---
+++ tests/ZLogger.MessagePack.Tests/FormatterTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MessagePack;
+using MessagePack.Resolvers;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using ZLogger.MessagePack;
+
+namespace ZLogger.Tests
+{
+    class TestException : Exception
+    {
+        public TestException(string message) : base(message)
+        {
+        }
+    }
+
+    class TestPayload
+    {
+        public int X { get; set; }
+    }
+    
+    class TestProcessor : IAsyncLogProcessor
+    {
+        public Queue<dynamic> EntryMessages = new Queue<dynamic>();
+        readonly ZLoggerOptions options;
+        readonly IZLoggerFormatter formatter;
+        readonly ArrayBufferWriter<byte> bufferWriter = new ArrayBufferWriter<byte>();
+
+        public dynamic Dequeue() => EntryMessages.Dequeue();
+
+        public TestProcessor(ZLoggerOptions options)
+        {
+            this.options = options;
+            formatter = options.CreateFormatter();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return default;
+        }
+
+        public void Post(IZLoggerEntry log)
+        {
+            log.FormatUtf8(bufferWriter, formatter);
+            var entry = MessagePackSerializer.Deserialize<dynamic>(bufferWriter.WrittenMemory, ContractlessStandardResolver.Options);
+            bufferWriter.Clear();
+            EntryMessages.Enqueue(entry);
+        }
+    }
+    
+    public class FormatterTest
+    {
+        TestProcessor processor;
+        ILogger logger;
+
+        public FormatterTest()
+        {
+            var options = new ZLoggerOptions();
+            options.UseMessagePackFormatter();
+
+            processor = new TestProcessor(options);
+
+            var loggerFactory = LoggerFactory.Create(x =>
+            {
+                x.SetMinimumLevel(LogLevel.Debug);
+                x.AddZLoggerLogProcessor(processor);
+            });
+            logger = loggerFactory.CreateLogger("test");
+        }
+
+        [Fact]
+        public void PlainMessage()
+        {
+            logger.ZLogInformation(new EventId(1, "HOGE"), "AAA {0} BBB {1}", 111, "Hello");
+            var msgpack = processor.Dequeue();
+            ((string)msgpack["CategoryName"]).Should().Be("test");
+            ((string)msgpack["LogLevel"]).Should().Be("Information");
+            ((int)msgpack["EventId"]).Should().Be(1);
+            ((string)msgpack["EventIdName"]).Should().Be("HOGE");
+            ((string)msgpack["Message"]).Should().Be("AAA 111 BBB Hello");
+            ((string)msgpack["Timestamp"]).Should().NotBeEmpty();
+            ((bool)msgpack.ContainsKey("Exception")).Should().BeFalse();
+            ((bool)msgpack.ContainsKey("Payload")).Should().BeFalse();
+        }
+
+        [Fact]
+        public void WithException()
+        {
+            try
+            {
+                throw new TestException("DAME");
+            }
+            catch (Exception ex)
+            {
+                logger.ZLogError(new EventId(1, "NG"), ex, "DAMEDA {0}", 111);                
+            }
+        
+            var msgpack = processor.Dequeue();
+            ((string)msgpack["CategoryName"]).Should().Be("test");
+            ((string)msgpack["LogLevel"]).Should().Be("Error");
+            ((int)msgpack["EventId"]).Should().Be(1);
+            ((string)msgpack["EventIdName"]).Should().Be("NG");
+            ((string)msgpack["Message"]).Should().Be("DAMEDA 111");
+            ((string)msgpack["Timestamp"]).Should().NotBeEmpty();
+            ((string)msgpack["Exception"]["Name"]).Should().Be("ZLogger.Tests.TestException");
+            ((string)msgpack["Exception"]["Message"]).Should().Be("DAME");
+            ((string)msgpack["Exception"]["StackTrace"]).Should().NotBeEmpty();
+            ((bool)msgpack.ContainsKey("Payload")).Should().BeFalse();
+        }
+    
+        [Fact]
+        public void WithPayload()
+        {
+            logger.ZLogInformationWithPayload(new EventId(1, "HOGE"), new TestPayload { X = 999}, "UMU {0}", 111);
+        
+            var msgpack = processor.Dequeue();
+            ((string)msgpack["CategoryName"]).Should().Be("test");
+            ((string)msgpack["LogLevel"]).Should().Be("Error");
+            ((int)msgpack["EventId"]).Should().Be(1);
+            ((string)msgpack["EventIdName"]).Should().Be("NG");
+            ((string)msgpack["Timestamp"]).Should().NotBeEmpty();
+            ((int)msgpack["Payload"]["X"]).Should().Be(999);
+            ((bool)msgpack.ContainsKey("Exception")).Should().BeFalse();            
+        }
+   }
+}
