7bf8b689e86114fe29cd0c7c15b8b26e9cd993ab
hadashiA <dev@hadashikick.jp>
2023-11-02 18:42:25 +0900

    Add a bench about writing file

--- sandbox/Benchmark/Benchmarks/PostLogEntry.cs
+++ sandbox/Benchmark/Benchmarks/PostLogEntry.cs
@@ -9,14 +9,15 @@ using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Benchmark.Benchmarks;
 
-public class CallLog
+[MemoryDiagnoser]
+public class PostLogEntry
 {
+    static readonly string NullDevicePath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "NUL" : "/dev/null";
+    
     ILogger zLogger = default!;
     ILogger serilogMsExtLogger = default!;
     ILogger nLogMsExtLogger = default!;
 
-    static string NullDevicePath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "NUL" : "/dev/null";
-    
     [GlobalSetup]
     public void SetUp()
     {
@@ -27,7 +28,7 @@ public class CallLog
             logging.AddZLoggerStream(Stream.Null);
         });
 
-        zLogger = zLoggerFactory.CreateLogger<CallLog>();
+        zLogger = zLoggerFactory.CreateLogger<PostLogEntry>();
         
         // Serilog
         
@@ -40,12 +41,15 @@ public class CallLog
             logging.AddSerilog(serilogLogger);
         });
         
-        serilogMsExtLogger = serilogMsExtLoggerFactory.CreateLogger<CallLog>();
+        serilogMsExtLogger = serilogMsExtLoggerFactory.CreateLogger<PostLogEntry>();
         
         // NLog
 
         var nLogConfig = new NLog.Config.LoggingConfiguration();
-        var target = new NLog.Targets.NullTarget("Null");
+        var target = new NLog.Targets.FileTarget("Null")
+        {
+            FileName = NullDevicePath
+        };
         var asyncTarget = new NLog.Targets.Wrappers.AsyncTargetWrapper(target);
         nLogConfig.AddTarget(asyncTarget);
         nLogConfig.AddRuleForAllLevels(asyncTarget);
@@ -55,27 +59,27 @@ public class CallLog
             logging.AddNLog(nLogConfig);
         });
 
-        nLogMsExtLogger = nLogMsExtLoggerFactory.CreateLogger<CallLog>();
+        nLogMsExtLogger = nLogMsExtLoggerFactory.CreateLogger<PostLogEntry>();
     }
 
     [Benchmark]
     public void ZLogger_ZLog()
     {
-        var x = 10;
-        var y = 20;
-        var z = 30;
+        var x = 100;
+        var y = 200;
+        var z = 300;
         zLogger.ZLogInformation($"foo{x} bar{y} nazo{z}");
     }
 
     [Benchmark]
     public void Serilog_Log()
     {
-        serilogMsExtLogger.LogInformation($"foo{100} bar{200} nazo{300}");
+        serilogMsExtLogger.LogInformation("x={X} y={Y} z={Z}", 100, 200, 300);
     }
 
     [Benchmark]
     public void NLog_Log()
     {
-        nLogMsExtLogger.LogInformation($"foo{100} bar{200} nazo{300}");
+        nLogMsExtLogger.LogInformation("x={X} y={Y} z={Z}", 100, 200, 300);
     }
 }
---
+++ sandbox/Benchmark/Benchmarks/WriteToJsonFile.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using ZLogger;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+using NLog.Extensions.Logging;
+using Serilog.Formatting.Json;
+using ZLogger.Formatters;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace Benchmark.IOBenchmarks;
+
+class BenchmarkConfig : ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddJob(Job.ShortRun.WithWarmupCount(1).WithIterationCount(1));
+    }
+}
+
+[Config(typeof(BenchmarkConfig))]
+public class WriteJsonToFile
+{
+    const int N = 10_000;
+    static readonly string NullDevicePath = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "NUL" : "/dev/null";
+
+    readonly ILoggerFactory zloggerFactory;
+    readonly ILoggerFactory serilogMsExtLoggerFactory;
+    readonly ILoggerFactory nLogMsExtLoggerFactory;
+    readonly ILogger zlogger;
+    readonly ILogger serilogMsExtLogger;
+    readonly ILogger nLogMsExtLogger;
+    
+    public WriteJsonToFile()
+    {
+        var zLoggerFactory = LoggerFactory.Create(logging =>
+        {
+            logging.AddZLoggerFile(NullDevicePath, options =>
+            {
+                options.UseJsonFormatter();
+            });
+        });
+
+        zlogger = zLoggerFactory.CreateLogger<WriteJsonToFile>();
+        
+        // Serilog
+        
+        var serilogLogger = new LoggerConfiguration()
+            // .WriteTo.Async(a => a.File(new JsonFormatter(), NullDevicePath, buffered: true))
+            .WriteTo.File(NullDevicePath, buffered: true)
+            .CreateLogger();
+        
+        serilogMsExtLoggerFactory = LoggerFactory.Create(logging =>
+        {
+            logging.AddSerilog(serilogLogger);
+        });
+        
+        serilogMsExtLogger = serilogMsExtLoggerFactory.CreateLogger<WriteJsonToFile>();
+        
+        // NLog
+
+        var nLogConfig = new NLog.Config.LoggingConfiguration();
+        var target = new NLog.Targets.FileTarget("File")
+        {
+            FileName = NullDevicePath,
+            BufferSize = 32768 // default 
+        };
+        // var asyncTarget = new NLog.Targets.Wrappers.AsyncTargetWrapper(target);
+        nLogConfig.AddTarget(target);
+        nLogConfig.AddRuleForAllLevels(target);
+
+        nLogMsExtLoggerFactory = LoggerFactory.Create(logging =>
+        {
+            logging.AddNLog(nLogConfig);
+        });
+
+        nLogMsExtLogger = nLogMsExtLoggerFactory.CreateLogger<WriteJsonToFile>();
+    }
+
+    [Benchmark]
+    public void ZLogger_WriteJsonToFile()
+    {
+        var x = 100;
+        var y = 200;
+        var z = 300;
+        for (var i = 0; i < N; i++)
+        {
+            zlogger.LogInformation($"x={x} y={y} z={z}");
+        }
+        zloggerFactory.Dispose(); // wait
+    }
+
+    [Benchmark]
+    public void Serilog_WriteJsonToFile()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            serilogMsExtLogger.LogInformation("x={X} y={Y} z={Z}", 100, 200, 300);
+        }
+        serilogMsExtLoggerFactory.Dispose(); // wait
+    }
+
+    [Benchmark]
+    public void NLog_WriteJsonToFile()
+    {
+        for (var i = 0; i < N; i++)
+        {
+            nLogMsExtLogger.LogInformation("x={X} y={Y} z={Z}", 100, 200, 300);
+        }
+        nLogMsExtLoggerFactory.Dispose(); // wait
+    }
+}
