6be394be2c76113bcd0d3c949fd0b39429ba9ad2
hadashiA <dev@hadashikick.jp>
2023-11-02 18:42:25 +0900

    Add a bench about writing file

--- sandbox/Benchmark/Program.cs
+++ sandbox/Benchmark/Program.cs
@@ -1,60 +1,30 @@
-﻿using Benchmark.Benchmarks;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnosers;
-using BenchmarkDotNet.Jobs;
+﻿using System.Reflection;
+using Benchmark.Benchmarks;
+using Benchmark.IOBenchmarks;
 using BenchmarkDotNet.Running;
-using System.Reflection;
-using System.Threading;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
 using Serilog;
+using Serilog.Formatting.Json;
+using ZLogger;
+using ZLogger.Formatters;
 
-namespace Benchmark
-{
-    // internal class BenchmarkConfig : ManualConfig
-    // {
-    //     public BenchmarkConfig()
-    //     {
-    //         Add(MemoryDiagnoser.Default);
-    //         Add(Job.ShortRun.WithWarmupCount(1).WithIterationCount(1));
-    //     }
-    // }
+BenchmarkSwitcher.FromAssembly(Assembly.GetEntryAssembly()!).Run(args);
 
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            BenchmarkSwitcher.FromAssembly(Assembly.GetEntryAssembly()).Run(args);
-            
-            // var serilogLogger = new LoggerConfiguration()
-            //     // .WriteTo.Async(a => a.File("/dev/null", buffered: true))
-            //     .WriteTo.Async(a => a.Console())
-            //     .CreateLogger();
-            //
-            // var serilogMsExtLoggerFactory = LoggerFactory.Create(logging =>
-            // {
-            //     logging.AddSerilog(serilogLogger);
-            // });
-            //
-            // var serilogMsExtLogger = serilogMsExtLoggerFactory.CreateLogger<CallLog>();
-            // serilogMsExtLogger.LogInformation($"SESESESSESEEE");
-            //
-            // var nLogConfig = new NLog.Config.LoggingConfiguration();
-            // var target = new NLog.Targets.ConsoleTarget("Null");
-            // var asyncTarget = new NLog.Targets.Wrappers.AsyncTargetWrapper(target);
-            // nLogConfig.AddTarget(asyncTarget);
-            // nLogConfig.AddRuleForAllLevels(asyncTarget);
-            //
-            // var nLogMsExtLoggerFactory = LoggerFactory.Create(logging =>
-            // {
-            //     logging.AddNLog(nLogConfig);
-            // });
-            //
-            // var nLogMsExtLogger = nLogMsExtLoggerFactory.CreateLogger<CallLog>();
-            //
-            // nLogMsExtLogger.LogInformation($"NNNNNNNNNNNN");
-            //
-            // nLogMsExtLoggerFactory.Dispose();
-        }
-    }
-}
+// var nLogConfig = new NLog.Config.LoggingConfiguration();
+// var target = new NLog.Targets.FileTarget("Null")
+// {
+//     FileName = "/dev/null"
+// };
+// var asyncTarget = new NLog.Targets.Wrappers.AsyncTargetWrapper(target);
+// nLogConfig.AddTarget(asyncTarget);
+// nLogConfig.AddRuleForAllLevels(asyncTarget);
+//
+// var nLogMsExtLoggerFactory = LoggerFactory.Create(logging =>
+// {
+//     logging.AddNLog(nLogConfig);
+// });
+//
+// var nLogMsExtLogger = nLogMsExtLoggerFactory.CreateLogger<PostLogEntry>();
+//
+// nLogMsExtLogger.LogInformation("{X}", 1);
