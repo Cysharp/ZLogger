feff91323dab2278cb25eaf0d5871918a03ff656
neuecc <ils@neue.cc>
2023-12-04 18:18:49 +0900

    impl done

--- tests/ZLogger.Generator.Tests/TestHelper.cs
+++ tests/ZLogger.Generator.Tests/TestHelper.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace ZLogger.Generator.Tests;
@@ -8,28 +7,31 @@ public static class TestHelper
 {
     public static IDisposable CreateMessageLogger<T>(out ILogger<T> logger, out List<string> messages)
     {
-        var list = new List<string>();
-        var factory = LoggerFactory.Create(logging =>
-        {
-            logging.AddZLoggerInMemory(processor =>
-            {
-                processor.MessageReceived += msg => list.Add(msg);
-            });
-        });
-
-        logger = factory.CreateLogger<T>();
-        messages = list;
-        return factory;
+        return CreateLogger(x => { }, out logger, out messages);
     }
 
     public static IDisposable CreateJsonLogger<T>(out ILogger<T> logger, out List<string> messages)
     {
+        return CreateLogger(x => x.UseJsonFormatter(formatter =>
+        {
+            formatter.IncludeProperties = IncludeProperties.ParameterKeyValues;
+        }), out logger, out messages);
+    }
+
+    public static IDisposable CreateLogger<T>(Action<ZLoggerOptions> configure, out ILogger<T> logger, out List<string> messages)
+    {
+        return CreateLogger(LogLevel.Trace, configure, out logger, out messages);
+    }
+
+    public static IDisposable CreateLogger<T>(LogLevel minimumLevel, Action<ZLoggerOptions> configure, out ILogger<T> logger, out List<string> messages)
+    {
         var list = new List<string>();
         var factory = LoggerFactory.Create(logging =>
         {
-            logging.AddZLoggerInMemory((options, provider) =>
+            logging.SetMinimumLevel(minimumLevel);
+            logging.AddZLoggerInMemory((options, services) =>
             {
-                options.UseJsonFormatter(formatter => formatter.IncludeProperties = IncludeProperties.ParameterKeyValues);
+                configure(options);
             }, processor =>
             {
                 processor.MessageReceived += msg => list.Add(msg);
