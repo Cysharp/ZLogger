90b938c9f4b432aa99cd29cfeb2bdca3f2a069cd
hadashiA <dev@hadashikick.jp>
2023-12-21 15:34:52 +0900

    Add loglevel variation

--- src/ZLogger.Unity/Assets/ZLogger.Unity/Runtime/UnityLoggerExtensions.cs
+++ src/ZLogger.Unity/Assets/ZLogger.Unity/Runtime/UnityLoggerExtensions.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 using ZLogger.LogStates;
@@ -9,7 +10,8 @@ public static class UnityLoggerExtensions
 	[UnityEngine.HideInCallstack]
     public static void ZLog(
         this ILogger logger, 
-        LogLevel logLevel, 
+        LogLevel logLevel,
+        Exception? exception,
         [InterpolatedStringHandlerArgument("logger", "logLevel")] ref ZLoggerInterpolatedStringHandler message, 
         UnityEngine.Object context)
     {
@@ -19,13 +21,86 @@ public static class UnityLoggerExtensions
 			var unityState = new UnityDebugLogState(state, context);
             try
             {
-                logger.Log(logLevel, default, unityState, null, static (state, ex) => state.ToString());
+                logger.Log(logLevel, default, unityState, exception, static (state, ex) => state.ToString());
             }
             finally
             {
                 state.Release();
             }
         }
+    }
+    
+    [UnityEngine.HideInCallstack]
+    public static void ZLog(this ILogger logger, LogLevel logLevel, [InterpolatedStringHandlerArgument("logger", "logLevel")] ref ZLoggerInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, logLevel, null, ref message, context);
+    }
+
+    [UnityEngine.HideInCallstack]
+    public static void ZLogTrace(this ILogger logger, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerTraceInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Trace, ref message.InnerHandler, context);
+    }
+ 
+    [UnityEngine.HideInCallstack]
+    public static void ZLogTrace(this ILogger logger, Exception? exception, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerTraceInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Trace, exception, ref message.InnerHandler, context);
+    }
+    [UnityEngine.HideInCallstack]
+    public static void ZLogDebug(this ILogger logger, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerDebugInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Debug, ref message.InnerHandler, context);
+    }
+ 
+    [UnityEngine.HideInCallstack]
+    public static void ZLogDebug(this ILogger logger, Exception? exception, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerDebugInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Debug, exception, ref message.InnerHandler, context);
+    }
+    [UnityEngine.HideInCallstack]
+    public static void ZLogInformation(this ILogger logger, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerInformationInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Information, ref message.InnerHandler, context);
+    }
+ 
+    [UnityEngine.HideInCallstack]
+    public static void ZLogInformation(this ILogger logger, Exception? exception, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerInformationInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Information, exception, ref message.InnerHandler, context);
+    }
+    [UnityEngine.HideInCallstack]
+    public static void ZLogWarning(this ILogger logger, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerWarningInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Warning, ref message.InnerHandler, context);
+    }
+ 
+    [UnityEngine.HideInCallstack]
+    public static void ZLogWarning(this ILogger logger, Exception? exception, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerWarningInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Warning, exception, ref message.InnerHandler, context);
+    }
+    [UnityEngine.HideInCallstack]
+    public static void ZLogError(this ILogger logger, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerErrorInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Error, ref message.InnerHandler, context);
+    }
+ 
+    [UnityEngine.HideInCallstack]
+    public static void ZLogError(this ILogger logger, Exception? exception, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerErrorInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Error, exception, ref message.InnerHandler, context);
+    }
+    [UnityEngine.HideInCallstack]
+    public static void ZLogCritical(this ILogger logger, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerCriticalInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Critical, ref message.InnerHandler, context);
+    }
+ 
+    [UnityEngine.HideInCallstack]
+    public static void ZLogCritical(this ILogger logger, Exception? exception, [InterpolatedStringHandlerArgument("logger")] ref ZLoggerCriticalInterpolatedStringHandler message, UnityEngine.Object context)
+    {
+        ZLog(logger, LogLevel.Critical, exception, ref message.InnerHandler, context);
     }
 }
 
--- src/ZLogger/ZLoggerInterpolatedStringHandler.LogLevel.cs
+++ src/ZLogger/ZLoggerInterpolatedStringHandler.LogLevel.cs
@@ -7,186 +7,186 @@ namespace ZLogger;
 [InterpolatedStringHandler]
 public ref struct ZLoggerTraceInterpolatedStringHandler
 {
-    internal ZLoggerInterpolatedStringHandler innerHandler;
+    public ZLoggerInterpolatedStringHandler InnerHandler;
 
     public ZLoggerTraceInterpolatedStringHandler(int literalLength, int formattedCount, ILogger logger, out bool enabled)
     {
-        this.innerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Trace, out enabled);
+        InnerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Trace, out enabled);
     }
     public void AppendLiteral(string s)
     {
-        this.innerHandler.AppendLiteral(s);
+        InnerHandler.AppendLiteral(s);
     }
 
     public void AppendFormatted<T>(T value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
     {
-        this.innerHandler.AppendFormatted(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>(Nullable<T> value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : struct
     {
-        this.innerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>((string, T) namedValue, int alignment = 0, string? format = null, string? _ = null)
     {
-        this.innerHandler.AppendFormatted(namedValue, alignment, format);
+        InnerHandler.AppendFormatted(namedValue, alignment, format);
     }
 }
 
 [InterpolatedStringHandler]
 public ref struct ZLoggerDebugInterpolatedStringHandler
 {
-    internal ZLoggerInterpolatedStringHandler innerHandler;
+    public ZLoggerInterpolatedStringHandler InnerHandler;
 
     public ZLoggerDebugInterpolatedStringHandler(int literalLength, int formattedCount, ILogger logger, out bool enabled)
     {
-        this.innerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Debug, out enabled);
+        InnerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Debug, out enabled);
     }
     public void AppendLiteral(string s)
     {
-        this.innerHandler.AppendLiteral(s);
+        InnerHandler.AppendLiteral(s);
     }
 
     public void AppendFormatted<T>(T value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
     {
-        this.innerHandler.AppendFormatted(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>(Nullable<T> value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : struct
     {
-        this.innerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>((string, T) namedValue, int alignment = 0, string? format = null, string? _ = null)
     {
-        this.innerHandler.AppendFormatted(namedValue, alignment, format);
+        InnerHandler.AppendFormatted(namedValue, alignment, format);
     }
 }
 
 [InterpolatedStringHandler]
 public ref struct ZLoggerInformationInterpolatedStringHandler
 {
-    internal ZLoggerInterpolatedStringHandler innerHandler;
+    public ZLoggerInterpolatedStringHandler InnerHandler;
 
     public ZLoggerInformationInterpolatedStringHandler(int literalLength, int formattedCount, ILogger logger, out bool enabled)
     {
-        this.innerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Information, out enabled);
+        InnerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Information, out enabled);
     }
     public void AppendLiteral(string s)
     {
-        this.innerHandler.AppendLiteral(s);
+        InnerHandler.AppendLiteral(s);
     }
 
     public void AppendFormatted<T>(T value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
     {
-        this.innerHandler.AppendFormatted(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>(Nullable<T> value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : struct
     {
-        this.innerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>((string, T) namedValue, int alignment = 0, string? format = null, string? _ = null)
     {
-        this.innerHandler.AppendFormatted(namedValue, alignment, format);
+        InnerHandler.AppendFormatted(namedValue, alignment, format);
     }
 }
 
 [InterpolatedStringHandler]
 public ref struct ZLoggerWarningInterpolatedStringHandler
 {
-    internal ZLoggerInterpolatedStringHandler innerHandler;
+    public ZLoggerInterpolatedStringHandler InnerHandler;
 
     public ZLoggerWarningInterpolatedStringHandler(int literalLength, int formattedCount, ILogger logger, out bool enabled)
     {
-        this.innerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Warning, out enabled);
+        InnerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Warning, out enabled);
     }
     public void AppendLiteral(string s)
     {
-        this.innerHandler.AppendLiteral(s);
+        InnerHandler.AppendLiteral(s);
     }
 
     public void AppendFormatted<T>(T value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
     {
-        this.innerHandler.AppendFormatted(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>(Nullable<T> value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : struct
     {
-        this.innerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>((string, T) namedValue, int alignment = 0, string? format = null, string? _ = null)
     {
-        this.innerHandler.AppendFormatted(namedValue, alignment, format);
+        InnerHandler.AppendFormatted(namedValue, alignment, format);
     }
 }
 
 [InterpolatedStringHandler]
 public ref struct ZLoggerErrorInterpolatedStringHandler
 {
-    internal ZLoggerInterpolatedStringHandler innerHandler;
+    public ZLoggerInterpolatedStringHandler InnerHandler;
 
     public ZLoggerErrorInterpolatedStringHandler(int literalLength, int formattedCount, ILogger logger, out bool enabled)
     {
-        this.innerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Error, out enabled);
+        InnerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Error, out enabled);
     }
     public void AppendLiteral(string s)
     {
-        this.innerHandler.AppendLiteral(s);
+        InnerHandler.AppendLiteral(s);
     }
 
     public void AppendFormatted<T>(T value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
     {
-        this.innerHandler.AppendFormatted(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>(Nullable<T> value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : struct
     {
-        this.innerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>((string, T) namedValue, int alignment = 0, string? format = null, string? _ = null)
     {
-        this.innerHandler.AppendFormatted(namedValue, alignment, format);
+        InnerHandler.AppendFormatted(namedValue, alignment, format);
     }
 }
 
 [InterpolatedStringHandler]
 public ref struct ZLoggerCriticalInterpolatedStringHandler
 {
-    internal ZLoggerInterpolatedStringHandler innerHandler;
+    public ZLoggerInterpolatedStringHandler InnerHandler;
 
     public ZLoggerCriticalInterpolatedStringHandler(int literalLength, int formattedCount, ILogger logger, out bool enabled)
     {
-        this.innerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Critical, out enabled);
+        InnerHandler = new ZLoggerInterpolatedStringHandler(literalLength, formattedCount, logger, LogLevel.Critical, out enabled);
     }
     public void AppendLiteral(string s)
     {
-        this.innerHandler.AppendLiteral(s);
+        InnerHandler.AppendLiteral(s);
     }
 
     public void AppendFormatted<T>(T value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
     {
-        this.innerHandler.AppendFormatted(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>(Nullable<T> value, int alignment = 0, string? format = null, [CallerArgumentExpression("value")] string? argumentName = null)
         where T : struct
     {
-        this.innerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
+        InnerHandler.AppendFormatted<T>(value, alignment, format, argumentName);
     }
 
     public void AppendFormatted<T>((string, T) namedValue, int alignment = 0, string? format = null, string? _ = null)
     {
-        this.innerHandler.AppendFormatted(namedValue, alignment, format);
+        InnerHandler.AppendFormatted(namedValue, alignment, format);
     }
 }
 
